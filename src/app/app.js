b1ffc77b89367de68c8d83478b40d0317ce299f1
Hyunsang <asunhs@gmail.com>
2016-02-21 00:03:19 +0900

    DF 추가

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,24 @@
+var analyzer = require("./rule/rule.js");
+
 module.exports = angular.module('packApp', [
     'templates-html'
 ]).config(/* @ngInject */ function ($compileProvider, $httpProvider) {
     $compileProvider.debugInfoEnabled(false);
     $httpProvider.useApplyAsync(true);
+}).controller('LottoCtrl', /* @ngInject */ function ($scope, chartLast, periods) {
+
+    $scope.last = chartLast;
+    $scope.diff200 = getDiffMA(2,0);
+    $scope.diff50 = getDiffMA(1,0);
+
+    function getDiffMA(origin, target) {
+        return analyzer.analyzeDifference(origin, target);
+    }
+
+    function getMATitle(index) {
+        return periods[index] + 'MA BEST';
+    }
+
+    $scope.getMATitle = getMATitle;
+    $scope.getDiffMA = getDiffMA;
 });
--- src/app/directives/stackChart.js
+++ src/app/directives/stackChart.js
@@ -1,21 +1,22 @@
 var Chart = require("../lib/Chart.min.js");
 
 
-require('app').directive('stackChart', /* @ngInject */ function (chartOption, chartLast, periods) {
+require('app').directive('stackChart', /* @ngInject */ function (chartOption) {
     return {
         restrict: 'E',
         replace: true,
         scope: {
-            period: '@'
+            label: '@',
+            title: '@',
+            start: '=',
+            data: '='
         },
         templateUrl: "directives/stackChart.tpl.html",
         link: function (scope, element) {
 
             var canvas = element.find('canvas'),
                 ctx = canvas.get(0).getContext("2d"),
-                data = chartLast[scope.period];
-
-            scope.label = periods[scope.period];
+                data = scope.data;
 
             scope.top = data.datasets[0].data.map(function (n, i) {
                 return { n : n, i : i+1 };
@@ -23,7 +24,10 @@ require('app').directive('stackChart', /* @ngInject */ function (chartOption, ch
                 return r.n - l.n;
             }).slice(0,6);
 
-            new Chart(ctx).Bar(data, chartOption);
+            new Chart(ctx).Bar(data, _.defaults({
+                scaleStartValue: scope.start || 0,
+                scaleBeginAtZero: true
+            }, chartOption));
         }
     };
 });
